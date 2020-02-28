71a712ba353cbc63b3268e9dae12beb473ca3070
Goosefraba <edwardnkim.93@gmail.com>
2020-02-27 20:36:11 -0800

    - Home page structure is completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,8 @@ function TimeDisplay(){
   });
 
   return (
-    <div className="time-style">
-      Los Angeles, CA - {counter}
+    <div>
+      <p className="time-style">Los Angeles, CA - {counter}</p>
     </div>
   )
 }
@@ -25,9 +25,9 @@ function TimeDisplay(){
 function SocialNetworks(){
   return(
           <div className="social-style">
-                <a href='https://github.com/Goosefraba814/ek-portfolio'><img style={{width: "5vh", height: "5vh", "padding-left": "2vh" }} src='https://img.icons8.com/bubbles/2x/github.png' alt="Github Icon"/></a>
-                <a href='https://www.linkedin.com/in/edward-kim-a77608106/'><img style={{width: "5vh", height: "5vh", "padding-left": "2vh" }} src='https://pngimg.com/uploads/linkedIn/linkedIn_PNG38.png' alt="Linkedin Icon"/></a>
-                <a href='/images/Resume_Edward_Kim.pdf' download><img style={{width: "5vh", height: "5vh", "padding-left": "2vh"}} src='../images/Resume_Icon.png' alt="Resume Download Icon"/></a>
+                <a href='https://github.com/Goosefraba814/ek-portfolio'><img src='https://img.icons8.com/bubbles/2x/github.png' alt="Github Icon"/></a>
+                <a href='https://www.linkedin.com/in/edward-kim-a77608106/'><img src='https://pngimg.com/uploads/linkedIn/linkedIn_PNG38.png' alt="Linkedin Icon"/></a>
+                <a href='/images/Resume_Edward_Kim.pdf' download><img src='../images/Resume_Icon.png' alt="Resume Download Icon"/></a>
           </div>
   )
 }
@@ -35,12 +35,12 @@ const Header = () => (
     <div className="Header">
         <TimeDisplay/>
         
-        <div className="link-style">
-            <Link to="/">Home</Link>
-            <Link to="/experience">Experience</Link>
-            <Link to="/project">Projects</Link>
-            <Link to="/contact">Contact</Link>
-        </div> 
+        <div className="link-container">
+            <Link className="link-style" to="/">Home</Link>
+            <Link className="link-style" to="/experience">Experience</Link>
+            <Link className="link-style" to="/project">Projects</Link>
+            <Link className="link-style" to="/contact">Contact</Link>
+        </div>
         <SocialNetworks/>
     </div>
 )
