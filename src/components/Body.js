a35d1c88aba1487f88c44b76ce9e52d4114266b6
Goosefraba <edwardnkim.93@gmail.com>
2020-02-27 22:17:38 -0800

    - Last more touch

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Experience from './Experience';
+import Projects from './Projects';
 
 import '../App.css'
 
@@ -13,6 +14,7 @@ const Body = () => (
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/experience" component={Experience} />
+                <Route path="/projects" component={Projects} />
             </Switch>
 
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+import '../css/Home.css'
+
+
+const RightSide = () => { 
+    const [index, setIndex] = useState(0);
+
+    var introduction = {
+        id: 0,
+        Questions: [
+            "Cats or Dogs?",
+            "hi1sdafjlkdsafjlskdafjlskdafjldsaifjosajfpdsaijfpsdafjpsdajfpsdjfpsdfjpdsfjpsdjfpsdjf dsfjpsdjfs pdsjf dsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjf789",
+        ],
+        Introduction: [
+            "I am a...."
+        ],
+    };
+    var interets = {
+        id: 1,
+        Questions: [
+            "Cats or Dogs?",
+            "hi1sdafjlkdsafjlskdafjlskdafjldsaifjosajfpdsaijfpsdafjpsdajfpsdjfpsdfjpdsfjpsdjfpsdjf dsfjpsdjfs pdsjf dsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjf789",
+        ],
+        Answers: [
+            "It would be dog",
+            "Yes, dafiasfsf",
+        ],
+    };
+    
+    const RightHeader = [introduction, interets];
+
+    return (
+        <div style={{height: "100%"}}>
+            <div className="right-header-component">
+                <p onClick={() => setIndex(0)} className={index === 0 ? '' : 'notSelected'} > haa </p>  
+                <p onClick={() => setIndex(1)} className={index === 1 ? '' : 'notSelected'}> hee </p>  
+            </div>
+
+            <div className="right-body-component">
+                body <br/>
+                <p>
+                {RightHeader[index].Questions.map((i) =>
+                            i 
+                )}
+                </p>
+            </div>
+        </div>
+    )
+}
+
 
 const Home = () => (    
-    <div>
-        
-        
+    <div style={{borderStyle:"solid", borderColor: "silver", height: "100%"}}>
+        <div className="main">
+            <div className="left">
+                <p>Hello World from Aquarium!</p>
+                <img src="/images/EK_Aquarium.jpg" alt="This is my face" />           
+            </div>
+
+            <div className="right">
+                <RightSide/>
+            </div>
+        </div>
     </div>
 )
 
