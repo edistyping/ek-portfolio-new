e7644c740a2a152ad424b082fa131e24c85168e9
Goosefraba <edwardnkim.93@gmail.com>
2020-03-01 19:08:56 -0800

    - Home structure is completed. Need formatting now

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,52 +3,64 @@ import React, { useState } from 'react';
 
 import '../css/Home.css'
 
+const Intro = () => {
+        var Skills = [
+            "SQL/NoSQL",
+            "VBA/Macro",
+            "C++",
+            "Java",
+            "Javascript (React)",
+            "OOP/Waterfall/Agile"]
+
+    return (
+        <div>
+                <p>My Name is Edward and I'm a graduate from UC Merced in Computer Science and Engineering</p>
+                <p>My Goal is to earn a full-time position as a web designer and developer using modern tools such as ReactJS.</p>
+                <p>I currently live in Los Angeles working as a Business System Anaylst and continue to search and develop new skills and interests.</p>
+              
+                <div className="right-skills"> 
+                    Skills here:
+                    {Skills.map((i) =>
+                        <p>{i}</p>            
+                    )}
+                </div>
+        </div>
+    )
+}
+
+const Interest = () => {
+    var QandA = ["Cats or Dogs?",
+            "Dogs but dog-like cat is also acceptable",
+            "What is your favorite albums or playlist?",
+            "Jazz or random mix"];
+
+    return (
+        <div>
+            {QandA.map((i) =>
+                <p>{i}</p>            
+            )}
+        </div>
+
+    )
+}
 
 const RightSide = () => { 
     const [index, setIndex] = useState(0);
 
-    var introduction = {
-        id: 0,
-        Questions: [
-            "Cats or Dogs?",
-            "hi1sdafjlkdsafjlskdafjlskdafjldsaifjosajfpdsaijfpsdafjpsdajfpsdjfpsdfjpdsfjpsdjfpsdjf dsfjpsdjfs pdsjf dsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjf789",
-        ],
-        Introduction: [
-            "I am a...."
-        ],
-    };
-    var interets = {
-        id: 1,
-        Questions: [
-            "Cats or Dogs?",
-            "hi1sdafjlkdsafjlskdafjlskdafjldsaifjosajfpdsaijfpsdafjpsdajfpsdjfpsdfjpdsfjpsdjfpsdjf dsfjpsdjfs pdsjf dsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjfdsfjpsdjfs pdsjf789",
-        ],
-        Answers: [
-            "It would be dog",
-            "Yes, dafiasfsf",
-        ],
-    };
-    
-    const RightHeader = [introduction, interets];
-
     return (
         <div style={{height: "100%", display: "flex", "flex-direction": "column",}}>
             <div className="right-header-component">
-                <p onClick={() => setIndex(0)} className={index === 0 ? '' : 'notSelected'} > haa </p>  
-                <p onClick={() => setIndex(1)} className={index === 1 ? '' : 'notSelected'}> hee </p>  
+                <p onClick={() => setIndex(0)} className={index === 0 ? '' : 'notSelected'} > Introduction </p>  
+                <p onClick={() => setIndex(1)} className={index === 1 ? '' : 'notSelected'}> Q and A </p>  
             </div>
 
+            
             <div className="right-body-component">
-                body <br/>
-                <p>
-                {RightHeader[index].Questions.map((i) =>
-                            i 
-                )}
-                </p>
-
-                <div className="right-skills"> 
-                    Skills here:
-                </div>
+                {index === 0 ? 
+                    (<Intro/>) 
+                    :
+                    (<Interest/>) 
+                }
             </div>
 
 
