616b58fb7b8ae6f9817aa5ad101d8fd88612c69b
Goosefraba <edwardnkim.93@gmail.com>
2020-04-24 21:12:08 -0700

    -Contact: Replaced with textarea. Needs email submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,41 +2,66 @@ import React from 'react';
 
 import '../css/Contact.css'
 
-const Contact = () => {
+class Contact extends React.Component {
 
-
-    return (    
-        <div style={{borderColor: "silver", minHeight: "85vh"}}>
-            <div className="main-contact">
-                <div className="contact-left">
-                    <p>Contact Me</p>
-                    <p style={{fontSize: "0.5em"}}>Please leave any message or feedbacks.</p> 
-                    <p style={{fontSize: "0.5em"}}>Thank you!</p>
-                </div>
+    constructor(props){
+        super(props);
+        this.state = {
+            email: 'test@gmaiml.com',
+            subject: 'Good day to you Edward! ',
+            message: 'Message lalala',
+        };
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleChange3 = this.handleChange3.bind(this);
+    }
     
-                <form className="contact-right">
-                    <label>
-                    <p>Your Name</p>
-                    <input type="text" style={{width:"80%", height: "5vh",}}/>
-                    </label>
-
-                    <label>
-                    <p>Your Email</p>
-                    <input type="text" style={{width:"80%"}}/>
-                    </label>
-
-                    <label>
-                    <p>Message</p>
-                    <input type="text" style={{width: "80%", }}/>
-                    </label>
-                    
-                    <input type="submit" value="Send" style={{textAlign: "center", width: "40%", height: "8%", margin: "auto", backgroundColor: "lightskyblue"}}/>
-                </form>
+    handleChange1(event){
+        this.setState({email: event.target.value});
+    }
+    handleChange2(event){
+        this.setState({subject: event.target.value});
+    }
+    handleChange3(event){
+        this.setState({message: event.target.value});
+    }
 
 
+    render() {
+        return (    
+            <div style={{backgroundColor: "darkslategrey", minHeight: "90vh", overflow: "auto"}}>
+                <div className="main-contact">
+                    <div className="contact-left">
+                        <p>Contact Me</p>
+                        <p style={{fontSize: "0.5em"}}>Please leave any message or feedbacks.</p> 
+                        <p style={{fontSize: "0.5em"}}>Thank you!</p>
+                    </div>
+        
+                    <div className="contact-right">
+                    <form>
+                        <label>
+                        <p>Your Email</p>
+                        <textarea value={this.state.email} onChange={this.handleChange1}/>
+                        </label>
+
+                        <label>
+                        <p>Your Subject (Optional)</p>
+                        <textarea value={this.state.subject} onChange={this.handleChange2}/>
+                        </label>
+
+                        <label>
+                        <p>Message</p>
+                        <textarea style={{height: "15vh"}} value={this.state.message} onChange={this.handleChange3}/>
+                        
+                        </label>
+                        
+                        <input type="submit" value="Send" style={{textAlign: "center", width: "40%", height: "8%", margin: "5% auto", backgroundColor: "lightskyblue"}}/>
+                    </form>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Contact;
